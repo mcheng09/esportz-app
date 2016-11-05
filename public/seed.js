7784915f4c7ab4f484b4b1195cd494f23d1e3822
Michael Cheng <mcheng.oc@gmail.com>
2016-11-04 21:43:24 -0700

    Adds a seed file with dummy data

---
+++ public/seed.js
@@ -0,0 +1,58 @@
+// Teams
+
+var overwatchTeams = [{
+  "id": 1,
+  "name" : "EnVyUs",
+  "rank" : 1,
+  "rating" : 1421,
+  "region": "USA",
+  "link": "https://teamenvyus.com/"
+  "captain": "INTERNETHULK",
+  "roster": [
+    "INTERNETHULK",
+    "COCCO",
+    "chipshajen",
+    "harryhook",
+    "Taimou",
+    "Talespin"
+  ]
+},{
+  "id": 2,
+  "name" : "MisfitsGG",
+  "rank" : 2,
+  "rating" : 1355,
+  "region": "Europe",
+  "link": "http://teammisfits.gg/"
+  "captain": "Kryw",
+  "roster": [
+    "Kryw",
+    "Zebbosai",
+    "skipjack",
+    "SoOn",
+    "Nevix",
+    "Hidan"
+  ]
+}]
+
+// Players
+var overwatchPlayers = [{
+  "id": 1,
+  "gamer_tag": "INTERNETHULK",
+  "name": "Dennis Hawelka",
+  "team" : "EnVyUs"
+},{
+  "id": 2,
+  "gamer_tag": "COCCO",
+  "name": "Christian Jonsson",
+  "team" : "EnVyUs"
+},{
+  "id": 3,
+  "gamer_tag": "Kryw",
+  "name": "Johnathan Nobre",
+  "team" : "MisfitsGG"
+},{
+  "id": 4,
+  "gamer_tag": "skipjack",
+  "name": "Nicolas Rosada",
+  "team" : "MisfitsGG"
+}]
